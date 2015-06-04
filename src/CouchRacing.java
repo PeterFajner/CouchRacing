33085714b9dfd7658744a07d6495cc1155ffcbce
Peter Fajner <fajner1+Github@gmail.com>
2015-06-04 15:09:18 -0600

    Changes

--- src/CouchRacing.java
+++ src/CouchRacing.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.util.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Peter on 2015-06-01.
@@ -21,6 +24,7 @@ class Game extends JPanel
 	
 	final int[] SCREENSIZE = new int[]{1200, 800};
 	final int GROUNDLEVEL = 500;
+	final String res = "../res/";
 	
 	ArrayList<Cloud> clouds = new ArrayList<Cloud>();
 
@@ -37,24 +41,44 @@ class Game extends JPanel
     public Game()
     {
         this.frame = createFrame(SCREENSIZE[0], SCREENSIZE[1]);
-        couch = new Couch(new Point2D.Double(0, 0));
+        couch = new Couch(new Point2D.Double(0, 0)); // create the couch
+		
+		// set an image for the couch
+		BufferedImage couchImage;
+		try {
+			couchImage = ImageIO.read(new File("../res/leathersofa.png"));
+			couch.setImage(couchImage);
+		} catch (IOException e) {
+			System.out.println("How can the couch be real if its image isn't real?");
+		}
     }
 	
-	public Cloud createCloud(int[] xSpread, int[] ySpread)
+	public Cloud createCloud(int[] xSpread, int[] ySpread, BufferedImage image)
 	{
 		Random r = new Random();
 		int x = r.nextInt(xSpread[1] - xSpread[0]) + xSpread[0];
 		int y = r.nextInt(ySpread[1] - ySpread[0]) + ySpread[0];
-		return new Cloud(new Point2D.Double(x, y));
+		return new Cloud(new Point2D.Double(x, y), image);
 	}
 	
     public void gameLoop() {
         couch.applyForce(new Vector2(100, -500)); // apply initial slingshot force; initially hardcoded
 		// create some clouds
+		//String[] cloudImages = new String[]{"cloud1.png", "cloud2.gif", "cloud3.gif", "cloud4.gif"};
+		String[] cloudImages = new String[]{"leathersofa.png"};
 		for (int i = 0; i < 250; i++) {
-			Cloud cloud = createCloud(new int[]{-3000, 3000}, new int[]{-1000, 1000});
+			String imageChoice = cloudImages[new Random().nextInt(cloudImages.length)];
+			BufferedImage image = null;
+			try {
+				ImageIO.read(new File(res + imageChoice));
+			} catch (IOException e) {
+				System.out.println("How can clouds be real if their textures aren't real?");
+			}
+			Cloud cloud = createCloud(new int[]{-3000, 3000}, new int[]{-1000, 1000}, image);
 			clouds.add(cloud);
 		}
+		
+		// main loop
         try {
             while (true) {
                 couch.applyForce(new Vector2(0, 3)); // gravity
@@ -78,10 +102,38 @@ class Game extends JPanel
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.fillRect(frame.getWidth()/2, frame.getHeight()/2, 30, 30); // render couch in middle of screen
+				
+		// render couch in middle of screen
+		int xPos = frame.getWidth()/2;
+		int yPos = frame.getHeight()/2;
+		double scale = 0.1;
+		int width = (int)(couch.image.getWidth() * scale);
+		int height = (int)(couch.image.getHeight() * scale);
+		if (couch.image != null) {
+			g2d.drawImage(couch.image, xPos, yPos, width, height,  null);
+		} else {
+			g2d.fillRect(xPos, yPos, 30, 30);
+		}
+		
 		for (Cloud cloud : clouds) {
-			g2d.fillOval((int)(frame.getWidth()/2 + cloud.pos.getX() - couch.pos.getX()), (int)(frame.getHeight()/2 + cloud.pos.getY() - couch.pos.getY()), 40, 20);
+			//g2d.fillOval((int)(frame.getWidth()/2 + cloud.pos.getX() - couch.pos.getX()), (int)(frame.getHeight()/2 + cloud.pos.getY() - couch.pos.getY()), 40, 20);
+			int cloudxPos = (int)(frame.getWidth()/2 + cloud.pos.getX() - couch.pos.getX());
+			int cloudyPos = (int)(frame.getHeight()/2 + cloud.pos.getY() - couch.pos.getY());
+			g2d.drawImage(cloud.image, cloudxPos, cloudyPos, null);
 		}
         // for object in objectList, render object with offset to couch
     }
 }
+
+/**
+TODO
+
+collision checking
+air resistance
+cloud push down
+air draft push up
+cloud texture
+ground texture
+points
+FPS selector
+*/
