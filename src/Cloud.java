70bdd4af95cf425c66f627d6d0998c5c758e15dc
Peter Fajner <fajner1+Github@gmail.com>
2015-06-04 15:09:46 -0600

    Changes

--- src/Cloud.java
+++ src/Cloud.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 
 public class Cloud
 {
 	public Point2D pos;
+	BufferedImage image;
 
-	public Cloud(Point2D pos)
+	public Cloud(Point2D pos, BufferedImage image)
 	{
 		this.pos = pos;
+		this.image = image;
 	}
 }
